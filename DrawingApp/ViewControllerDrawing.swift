5fdb853b8c284c304c3d6a008b78e627b1626517
kelsey <kelsey.collins23@d155.org>
2023-05-04 23:04:58 -0500

    Work on the random and restart button

--- DrawingApp/ViewControllerDrawing.swift
+++ DrawingApp/ViewControllerDrawing.swift
@@ -7,32 +7,28 @@
 import PencilKit
 import UIKit
 
-class ViewControllerDrawing: UIViewController {
-    private let canvasView: PKCanvasView = {
-        let canvas = PKCanvasView()
-        //allows anyone to draw
-        canvas.drawingPolicy = .anyInput
-        return canvas
-    }()
-
+class ViewControllerDrawing: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
+    
+   
+    @IBOutlet weak var canvasView: PKCanvasView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(canvasView)
-
+        canvasView.drawingPolicy = .anyInput
+        canvasView.delegate = self
+        
+        if let window = parent?.view.window,
+           let toolPicker = PKToolPicker.shared(for: window){
+            toolPicker.addObserver(canvasView)
+            toolPicker.setVisible(true, forFirstResponder: canvasView)
+            canvasView.becomeFirstResponder()
+        }
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        canvasView.frame = view.bounds
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        let toolPicker = PKToolPicker()
-        toolPicker.setVisible(true, forFirstResponder: canvasView)
-        toolPicker.addObserver(canvasView)
-        canvasView.becomeFirstResponder()
-    }
-
+  
+    
+    
 }
